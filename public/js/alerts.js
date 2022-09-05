a263d39b43ecd588b7b32e6c4da6f0efc49b38bb
kipp <kipphulick@gmail.com>
2022-09-05 14:45:58 -0500

    finding where to use try catch blocks

--- public/js/alerts.js
+++ public/js/alerts.js
@@ -1,1 +1,9 @@
-console.log('Hi')
+let alerts = document.getElementsByClassName('alert')
+let alertMessage = document.getElementsByClassName('alertMessage')
+
+alertMessage[0].innerHTML = 'Error submitting alert'
+alerts[0].classList.toggle('active')
+setTimeout(() => {
+    alerts[0].classList.toggle('active')
+    alertMessage[0].innerHTML = ''
+}, 2000)
